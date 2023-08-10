cbf7de1d40003f05cf6468c6f430e866a993d2c7
Victor Garcia <victorgf2011@gmail.com>
2023-08-11 00:16:15 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/benibus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.benibus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/stop.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+class StopPage extends StatefulWidget {
+  const StopPage({Key? key, required this.title, required this.id})
+      : super(key: key);
+
+  final String title;
+  final String id;
+
+  @override
+  State<StopPage> createState() => _StopPageState();
+}
+
+class _StopPageState extends State<StopPage> {
+  List<dynamic> items = [];
+
+  void loadStop() async {
+    final responseItems = jsonDecode(((await http.get(Uri.parse(
+            'https://apisvt.avanzagrupo.com/lineas/getTraficosParada?empresa=5&parada=' +
+                widget.id)))
+        .body));
+
+    print(responseItems);
+
+    setState(() {
+      items.clear();
+      items.addAll(responseItems['data']['traficos']
+          .map((e) => e['coLinea'] + ' - ' + e['quedan']));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadStop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              loadStop();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+          );
+        },
+      ),
+    );
+  }
+}
